2951922b9822c6214f8f797d6d92e6bb04f67aa4
João Victor Moreira <jvrodriguesmoreira@gmail.com>
2021-01-07 08:29:56 -0300

    commit inicial

---
+++ src/clients/__test__/stormGlass.test.ts
@@ -0,0 +1,20 @@
+import { StormGlass } from '@src/clients/stormGlass';
+import axios from 'axios';
+import stormGlassWeather3HoursFixture from '@test/fixtures/stormglass_weather_3_hours.json';
+import stormGlassNormalized3HoursFixture from '@test/fixtures/stormglass_normalized_response_3_hours.json';
+
+jest.mock('axios');
+
+describe('StormGlass Client', () => {
+    it('should return the normalized forecast from the StormGlass service', async () => {
+        const lat = -22.498235;
+        const lng = -44.1264601;
+
+        axios.get = jest.fn().mockResolvedValue(stormGlassWeather3HoursFixture);
+
+        const stormGlass = new StormGlass(axios);
+        const response = await stormGlass.fetchPoints(lat, lng);
+
+        expect(response).toEqual(stormGlassNormalized3HoursFixture);
+    });
+});
